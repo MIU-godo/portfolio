b63ea32999c3c4003a55ca8386e64c28e72ea5f6
MIU-godo <86360147+MIU-godo@users.noreply.github.com>
2021-06-28 07:41:52 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(function(){
-	//$("button").html("Click");
+	$(".fold").hover(function(){
+		$(this).css("opacity",".5");
+	}, function(){
+		$(this).css("opacity","1");
+	});
+	// $(".fold").click(funtion(){
+		
+	// });
 });
