85d9398c199b5b82cc2257830f26a2a862eb5c9d
MIU-godo <86360147+MIU-godo@users.noreply.github.com>
2021-06-29 00:40:58 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 $(function(){
-	$(".fold").hover(function(){
-		$(this).css("opacity",".5");
+	$(".phone-size").hover(function(){
+		$(this).animate({"opacity":".5"}, 1000);
+		// $(this).css("opacity",".5");
 	}, function(){
-		$(this).css("opacity","1");
+		$(this).animate({"opacity":"1"}, 1000);
+		// $(this).css("opacity","1");
 	});
-	// $(".fold").click(funtion(){
-		
-	// });
+	$(".phone-size").click(function(){
+		$(".phone-list").slideToggle(100);
+	});
+	$(".close").click(function(){
+		$(".phone-list").slideToggle(100);
+	});
+
+	// スムーススクロール
+	$("a[href^=#]:not([href=#])").click(function(){
+		var target = $($(this).attr("href")).offset().top;
+		target -= 50;
+		$("html, body").animate({scrollTop: target}, 500);
+		return false;
+	})
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 100){
+			$(".for-top").fadeIn(500);
+		}else{
+			$(".for-top").fadeOut(500);
+		}
+	});
 });
